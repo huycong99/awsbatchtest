108532de892015c519fbe0d4ea78880891550025
unknown <huycong11099@gmail.com>
2024-10-17 22:59:42 +0700

    testsource

---
+++ TestAWSbatch/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace TestAWSbatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length==0)
+            {
+                Console.WriteLine("args not found");
+            }
+            else 
+            {
+                Console.WriteLine($"{args[0]}");
+            }
+            
+        }
+    }
+}
